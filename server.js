64421a62efb3546342197f26ccd3ed17c974a3f8
yerimechoi <yerimechoi@gmail.com>
2023-02-26 19:33:20 -0500

    Informed express.js on which template engine to use

--- server.js
+++ server.js
@@ -20,8 +20,12 @@ const sess = {
 
 app.use(session(sess));
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
